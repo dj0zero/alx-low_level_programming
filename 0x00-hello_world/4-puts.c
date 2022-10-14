a71b1894bc5f967022ab7ee94fe3bbece092fcc3
dj0zero <denzeljerry.dj@gmail.com>
2022-10-14 00:37:49 -0700

    C World commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Print a string using put
+ * main - print the string in the put function
  *
- * return: "Programming is like building a multilingual puzzle
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
  */
 
 int main (void)
