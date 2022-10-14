e6f5f87eeb29daa29e9a2fc22a8b74b9b901dce0
dj0zero <denzeljerry.dj@gmail.com>
2022-10-13 23:29:31 -0700

    C World commit

---
+++ l/0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string using put
+ *
+ * return: "Programming is like building a multilingual puzzle
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ l/0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string using printf
+ *
+ * return: "with proper grammar, but the outcome is a piece of art,
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
