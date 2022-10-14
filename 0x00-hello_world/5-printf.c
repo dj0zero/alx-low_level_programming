1d6fedbeb296063b304d1bd8bc465742d1aff78e
dj0zero <denzeljerry.dj@gmail.com>
2022-10-14 00:48:41 -0700

    C World commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Print a string using printf
+ * main - print the string in the print function
  *
- * return: "with proper grammar, but the outcome is a piece of art,
+ * Description: using the main function
+ * this program prints "with proper grammar, but the outcome is a piece of art,
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
