80e9c142896b90be95cc571e1cf6addfdc61f5c3
dj0zero <denzeljerry.dj@gmail.com>
2022-10-14 00:40:03 -0700

    C World commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  * this program prints "Programming is like building a multilingual puzzle
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
