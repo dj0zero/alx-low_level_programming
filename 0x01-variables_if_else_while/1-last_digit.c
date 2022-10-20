7487bfd0811453cc468e130932d8aa90d8553461
dj0zero <denzeljerry.dj@gmail.com>
2022-10-19 17:29:13 -0700

    Variables commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,11 +5,11 @@
 /**
  * main - prints the last digit of a random number
  * Description: using the main function
- * this program prints "Programming is greater than 5, is zero, or is less than 6 and not 0
+ * this program prints "Programming is greater, is zero, or is less
  * Return: 0
  */
 
-int main (void)
+int main(void)
 {
 	int n;
 	int s;
@@ -24,6 +24,5 @@ int main (void)
 		printf("Last digit of %d is %d and is 0\n", n, s);
 	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
-	
 	return (0);
 }
