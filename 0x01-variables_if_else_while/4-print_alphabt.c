95c2bbc0afa4a8e6d61fb34daf445df5502cb379
dj0zero <denzeljerry.dj@gmail.com>
2022-10-19 18:45:57 -0700

    Variables commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,17 +5,17 @@
  * followed by a new line
  * Return: 0
  */
- 
+
 int main(void)
 {
 char ch;
 
 for (ch = 'a' ; ch <= 'z' ; ch++)
-{	
-	if (ch != 'e' && ch != 'q')	
-	{
-			putchar(ch);		
-	}	
+{
+if (ch != 'e' && ch != 'q')	
+{
+	putchar(ch);		
+}	
 }
 putchar('\n');
 
