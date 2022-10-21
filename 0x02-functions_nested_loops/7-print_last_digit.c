ab2060ac06cd8298e679a63b2e3c5ec6eda690c1
dj0zero <denzeljerry.dj@gmail.com>
2022-10-21 07:24:01 -0700

    Functions commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - function prints the last digit of a number
- * c will be used for the function
+ * @c: will be used for the function
  * Return: 0
  */
 
@@ -11,15 +11,12 @@ int print_last_digit(int c)
 	if (c > 0 || c == 0)
 	{
 		_putchar (c % 10 + '0');
-
 		return (c % 10);
 	}
 	else
 	{
 		c = c * -1;
-
 		_putchar (c % 10 + '0');
-
 		return (c % 10);
 	}
 }
