d87529976b110e61d335166ad7e741b44404b2c1
dj0zero <denzeljerry.dj@gmail.com>
2022-10-26 12:24:24 -0700

    More functions commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - function checks for uppercase character
  * @c: will be used for character
- * Return: 0
+ * Return: 1 if uppercase, 0 if not
  */
 
 int _isupper(int c)
 {
 
-	if (c >= 65 && c <= 98)
+	if (c >= 65 && c <= 90)
 
 	{
 	return (1);
