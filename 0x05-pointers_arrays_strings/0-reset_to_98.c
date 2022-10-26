1154f37e8de98af16d864bd774602bbbc296ed13
dj0zero <denzeljerry.dj@gmail.com>
2022-10-25 19:38:36 -0700

    Pointers commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include "main.h"
-
 /**
  * reset_to_98 - function takes pointer to int
  * updating the value it points to 98
  * @n: will be used as declaration of n
  * Return: 0
  */
-
- void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
-
 	*n = 98;
-
 }
