a3f0f048e1b27c533c86cf848111de6168136a06
dj0zero <denzeljerry.dj@gmail.com>
2022-10-26 12:39:42 -0700

    More functions commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,8 +3,7 @@
 /**
  * _isdigit - function checks for digit between 0 and 9
  * @c: will be used for character
- * Return: 1 if c is a digit
- * otherwise 0
+ * Return: 1 if c is a digit, 0 if not 
  */
 
 int _isdigit(int c)
