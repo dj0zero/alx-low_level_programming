5a2cabd75f5a4fb68a16a60a0c2a6cab644b8bd6
dj0zero <denzeljerry.dj@gmail.com>
2022-10-31 06:19:01 -0700

    More functions commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 0; ch < 15; ch++)
+		for (ch = 0; ch <= 14; ch++)
 		{
 			if (ch >= 10)
-				_putchar((ch / 10) + 48);
-			_putchar((ch % 10) + 48);
+				_putchar((ch / 10) + '0');
+			_putchar((ch % 10) + '0');
 		}
 		_putchar('\n');
 	}
