d3f6d6c105f96e9e1d186122463edbefb6f20c25
dj0zero <denzeljerry.dj@gmail.com>
2022-11-09 03:28:44 -0800

    More functions commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - function prints a square
+ * followed by new line
+ * @size: will be square size
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int a = 0, d;
+
+	while (a < size && size > 0)
+	{
+		b = 0;
+		while (b < size)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		a++;
+	}
+	if a == 0;
+		_putchar('\n');
+}
+
