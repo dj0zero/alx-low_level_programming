9d6087c47150d2bd9674e23e820e883b0aeb7aff
dj0zero <denzeljerry.dj@gmail.com>
2022-11-09 05:01:49 -0800

    More functions commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - FizzBuzz code to print numbers from 1 to 100
+ * main - FizzBuzz function to print numbers from 1 to 100
  * Return: 0
  */
 
 int main(void)
 {
-	int a = 1;
+	int i;
 
-	while (a <= 100)
+	i = 1;
+	printf("%d", i);
+	
+	for (i = 2; i <= 100; i++)
 	{
-		if ((a % 3 == 0) && (a % 5 == 0))
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
-		else if (a % 3 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
-		else if (a % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d", a);
+			printf("%d", i);
 		}
-		if (a != 100)
-		{
-			putchar(' ');
-
-		}
-
-		a++;
 	}
-	_putchar('\n');
+	printf('\n')
 	return (0);
 }
