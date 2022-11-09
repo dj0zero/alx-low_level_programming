19c166632dc7c325333277d7e27597ae52c3a5e3
dj0zero <denzeljerry.dj@gmail.com>
2022-11-09 12:54:35 -0800

    Malloc commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,18 +23,18 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
-			len++;
+		len++;
 
-			concat_str = malloc(sizeof(char) * len);
+	concat_str = malloc(sizeof(char) * len);
 
-			if (concat_str == NULL)
-				return (NULL);
+	if (concat_str == NULL)
+		return (NULL);
 
-			for (index = 0; s1[index]; index++)
-				concat_str[concat_index++] = s1[index];
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
 
-			for (index = 0; s2[index]; index++)
-				concat_str[concat_index++] = s2[index];
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
 
-			return (concat_str);
+	return (concat_str);
 }
