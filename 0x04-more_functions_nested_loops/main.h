1d4d2882b84a3724182c7184a69ecc5199a3e79f
dj0zero <denzeljerry.dj@gmail.com>
2022-11-09 02:40:05 -0800

    More functions commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,6 +9,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(int character);
-
-#endif
