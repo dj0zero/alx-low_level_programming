3657262e99295ff7c24954823fc8e1b1e016562b
dj0zero <denzeljerry.dj@gmail.com>
2022-11-10 06:06:27 -0800

    ARGC commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that multiplies two numbers printing the product
+ * followed by new line
+ * @argc: will be the number of command line arguments
+ * @argv: will be array name
+ * Return: 1 if program doesn't recieve two arguments
+ * otherwise 0
+ */
+
+int main(int argc, char *argc[])
+{
+	int a, b;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		b = 1;
+
+		for (a = 1; a < 3; a++)
+			a *= atoi(argv[a]);
+
+		printf("%d\n", b);
+	}
+
+	return (0);
+}
