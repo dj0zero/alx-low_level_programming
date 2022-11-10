1fa22167e6161c492687fc72bfde1fad1cc693c0
dj0zero <denzeljerry.dj@gmail.com>
2022-11-10 05:40:05 -0800

    ARGC commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function prints the name of the program
+ * followed by new line
+ * @argv: will be the array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%S\n", argv[argc * 0]);
+
+	return (0);
+}
