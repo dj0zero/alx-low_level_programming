81c61849cf7adb8d4c54e73c4d50d5e632688dad
dj0zero <denzeljerry.dj@gmail.com>
2022-11-10 05:49:15 -0800

    ARGC commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include "msin.h"
+#include <stdio.h>
+
+/**
+ * main - function prints number of arguments passed into it
+ * @argc: will be number of command line arguemnts
+ * @argv: will be array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (a = 0; *argv; a++, argv++):
+
+		printf("%d\n", a - 1);
+	}
+
+	return (0);
+}
