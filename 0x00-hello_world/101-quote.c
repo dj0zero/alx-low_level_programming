b31751a4b1b35896b722e8c6c78e2cfae436248b
dj0zero <denzeljerry.dj@gmail.com>
2022-11-14 07:51:59 -0800

    Hello World commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - prints a line of words
+ * followed by new line
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str1[] = "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, strl, 59);
+	return (1);
+}
