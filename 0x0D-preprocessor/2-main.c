5481c6d885849d6cbd9551e8ad394bf69be25990
dj0zero <denzeljerry.dj@gmail.com>
2022-11-15 11:37:49 -0800

    Preprocessor commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/*
+ * main - function prints the name of the file
+ * that it was compiled from
+ * followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%d\n", __FILE__);
+
+	return (0);
+}
