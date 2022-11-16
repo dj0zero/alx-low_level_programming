f7ed71e5b9ea614724dbd421ee0be76e4894f1eb
dj0zero <denzeljerry.dj@gmail.com>
2022-11-16 12:11:38 -0800

    Structures commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,7 @@
 
 int _strlen(char *str);
 char *_strcopy(char *dest, char *src);
-dog_t *new_dog(cjhar *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
  * _strlen - function finds the legnth of a string
