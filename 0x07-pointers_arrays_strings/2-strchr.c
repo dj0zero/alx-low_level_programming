813347046b06acc6e0d329b349c0f12a4a101483
dj0zero <denzeljerry.dj@gmail.com>
2022-11-16 03:57:46 -0800

    Even more pointers commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - function locates a character in a string
+ * @s: will be the string
+ * @c: will be the character
+ * REturn: pointer to first character c occurence
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int b = 0;
+
+	for (; *(s + b) != '\0'; b++)
+		if (*(s + b) == c)
+			return (s + b);
+	if (*(s + b) == c)
+		return (s + b);
+	return ('\0');
+}
