226e53253dae691f24b442a27d22433697a2d357
dj0zero <denzeljerry.dj@gmail.com>
2022-11-15 16:10:21 -0800

    Structures commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,7 +28,7 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog t *new_dog(Char *name, float age, char *owner);
+dog t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
