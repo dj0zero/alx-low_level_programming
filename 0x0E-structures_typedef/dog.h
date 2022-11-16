c9937bdffbfa369a4557d379ca80d49439271dd9
dj0zero <denzeljerry.dj@gmail.com>
2022-11-16 12:08:40 -0800

    Structures commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * File: dog.h
- * Desc: Header file that defines a new type struct dog
- */
-
 /**
  * struct dog - A new type that describes a dog
  * @name: will be the dog's name
@@ -20,15 +15,10 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for the struct dog
- */
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner)
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
