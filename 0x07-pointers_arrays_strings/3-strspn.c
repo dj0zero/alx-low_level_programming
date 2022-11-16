2a61ffee16c07c1fd340b313be404dc940141d18
dj0zero <denzeljerry.dj@gmail.com>
2022-11-16 04:08:04 -0800

    Even more pointers commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (a = 0; *(s + a) != '\0' ; a++)
 	{
 		bool = 1;
-		for (j = 0; *(accept + b) != '\0'; b++)
+		for (b = 0; *(accept + b) != '\0'; b++)
 		{
 			if (*(s + a) == *(accept + b))
 			{
