9be426237cc4925d19779ae6cd314414b1a056a7
dj0zero <denzeljerry.dj@gmail.com>
2022-11-16 04:19:56 -0800

    Even more pointers commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches the string
+ * for any of set of byte
+ * @s: will be the first string
+ * @accept: will be the second string
+ * Return: pointer to byte in s matching a byte in accept
+ * otherwise NULL if no similar byte found
+ */
+
+char *strpbrk(char *s, char *accept)
+{
+	unsigned int c, d;
+
+	for (c = 0; *(s + c) != '\0'; c++)
+	{
+		for (d = 0; *(accept + d) != '\0'; d++)
+		{
+			if (*(s + c) == *(accept + d))
+				return (s + c);
+		}
+	}
+	return ('\0');
+}
