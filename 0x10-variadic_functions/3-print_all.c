621809b7b948af2dcee6992ed3087fdb3f4876b3
dj0zero <denzeljerry.dj@gmail.com>
2022-11-18 15:02:17 -0800

    Variadic commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,12 +9,12 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list num;
+	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
-	va_start(num, format);
+	va_start(valist, format);
 	while (format && format[i])
 	{
 	j = 0;
@@ -29,19 +29,19 @@ void print_all(const char * const format, ...)
 	switch (format[i])
 	{
 	case 'c':
-	printf("%c", va_arg(num, int)), c = 1;
+	printf("%c", va_arg(valist, int)), c = 1;
 	break;
 
 	case 'i':
-	printf("%d", va_arg(num, int)), c = 1;
+	printf("%d", va_arg(valist, int)), c = 1;
 	break;
 
 	case 'f':
-	printf("%f", va_arg(num, double)), c =1;
+	printf("%f", va_arg(valist, double)), c =1;
 	break;
 
 	case 's':
-	str = va_arg(num, char *), c =1;
+	str = va_arg(valist, char *), c =1;
 	if (!str)
 	{
 	printf("(nil)");
@@ -51,5 +51,5 @@ void print_all(const char * const format, ...)
 	break;
 	} i++;
 	}
-	printf("\n"), va_end(num);
+	printf("\n"), va_end(valist);
 }
